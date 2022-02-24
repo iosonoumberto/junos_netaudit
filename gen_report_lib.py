634acfcdce8b40ea9b5890c870dc2909c88e522f
UmbertoM <umberto.manferdini@gmail.com>
2022-02-24 17:50:45 +0100

    adjusted failure check on threshold test

--- gen_report_lib.py
+++ gen_report_lib.py
@@ -52,9 +52,9 @@ def threshold(scan, check):
         flag=1
         for tested in res_dict[check['cmd']]:
             if check['fail']=="lower":
-                good = int(res_dict[check['cmd']][tested][check['tfield']])<=threshold
-            else:
                 good = int(res_dict[check['cmd']][tested][check['tfield']])>=threshold
+            else:
+                good = int(res_dict[check['cmd']][tested][check['tfield']])<=threshold
             if not good:
                 if flag:
                     failed.append(res_dict['hostname'])
