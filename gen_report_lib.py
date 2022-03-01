e5b48ceb75fa9ee466200ca7d8e9538f5fb9c531
UmbertoM <umberto.manferdini@gmail.com>
2022-03-01 16:44:14 +0100

    added nodata list to print devices that had no data for a specific command (e.g. a device with no bfd will not have any data about bfd)

--- gen_report_lib.py
+++ gen_report_lib.py
@@ -18,6 +18,9 @@ def print_failures(desc, failed, failed_detail):
                     text+="\t" + str(k) + " - " + str(l[k]) + "\n"
                 text+="\t----\n"
     text+="\n"
+    if len(nodata) > 0:
+        text+="\n"
+        text+="The following devices had no data: " + str(nodata) + "\n"
     return text
 
 def print_distribution(desc, dfield, distr):
@@ -31,6 +34,9 @@ def print_distribution(desc, dfield, distr):
         for e in distr[dev]:
             text+="  " + e + " : " + str(distr[dev][e]) + " , " + str(float(distr[dev][e]/tot*100)) + "%\n"
         text+="\t----\n"
+    if len(nodata) > 0:
+        text+="\n"
+        text+="The following devices had no data: " + str(nodata) + "\n"
     return text
 
 def print_dict(desc, dict):
@@ -38,24 +44,34 @@ def print_dict(desc, dict):
     for x in dict:
         text+="  - " + x + " : " + str(dict[x]) + "\n"
         text+="\t----\n"
+    if len(nodata) > 0:
+        text+="\n"
+        text+="The following devices had no data: " + str(nodata) + "\n"
     return text
 
-def print_basic_stats(desc,unit, stats):
+def print_basic_stats(desc, unit, stats, nodata):
     text="TEST REPORT RESULT FOR " + desc + "\n\n"
     text+="MAX value -> device " + stats['maxv']['host'] + " : " + str(stats['maxv']['val']) + unit + "\n"
     text+="MIN value -> device " + stats['minv']['host'] + " : " + str(stats['minv']['val']) + unit + "\n"
     text+="AVG value -> " + str(stats['avg']) + unit + "\n"
+    if len(nodata) > 0:
+        text+="\n"
+        text+="The following devices had no data: " + str(nodata) + "\n"
     return text
 
 def string_equal(scan, check):
     failed=[]
     failed_detail={}
+    nodata=[]
     results = os.listdir(scan)
     for result in results:
         fr=open(scan+"/"+result,'r')
         res_dict=json.load(fr)
         fr.close()
         flag=1
+        if not res_dict[check['cmd']]:
+            nodata.append(res_dict['hostname'])
+            continue
         for tested in res_dict[check['cmd']]:
             if res_dict[check['cmd']][tested][check['tfield']]!=check['val']:
                 if flag:
@@ -63,7 +79,7 @@ def string_equal(scan, check):
                     failed_detail[res_dict['hostname']]=[]
                     flag=0
                 failed_detail[res_dict['hostname']].append(res_dict[check['cmd']][tested])
-    text=print_failures(check['desc'], failed, failed_detail)
+    text=print_failures(check['desc'], failed, failed_detail, nodata)
     return text
 
 def threshold(scan, check):
@@ -77,11 +93,15 @@ def threshold(scan, check):
 
     failed=[]
     failed_detail={}
+    nodata=[]
     results = os.listdir(scan)
     for result in results:
         fr=open(scan+"/"+result,'r')
         res_dict=json.load(fr)
         fr.close()
+        if not res_dict[check['cmd']]:
+            nodata.append(res_dict['hostname'])
+            continue
         if str(check['tfield']+'_thr') in models[res_dict['model']]:
             threshold=models[res_dict['model']][check['tfield']+'_thr']
         else:
@@ -101,16 +121,20 @@ def threshold(scan, check):
                     failed_detail[res_dict['hostname'] + ' thr: ' + str(threshold)]=[]
                     flag=0
                 failed_detail[res_dict['hostname'] + ' thr: ' + str(threshold)].append(res_dict[check['cmd']][tested])
-    text=print_failures(check['desc'], failed, failed_detail)
+    text=print_failures(check['desc'], failed, failed_detail, nodata)
     return text
 
 def distribution(scan, check):
     results = os.listdir(scan)
     distr={}
+    nodata=[]
     for result in results:
         fr=open(scan+"/"+result,'r')
         res_dict=json.load(fr)
         fr.close()
+        if not res_dict[check['cmd']]:
+            nodata.append(res_dict['hostname'])
+            continue
         host=res_dict['hostname']
         distr[host]={}
         distr_cmd=check['cmd']
@@ -120,20 +144,24 @@ def distribution(scan, check):
                 distr[host][res_dict[distr_cmd][e][dfield]]=1
             else:
                 distr[host][res_dict[distr_cmd][e][dfield]]+=1
-    text=print_distribution(check['desc'], dfield, distr)
+    text=print_distribution(check['desc'], dfield, distr, nodata)
     return text
 
 def total(scan, check):
     results = os.listdir(scan)
     tot_dict={}
+    nodata=[]
     for result in results:
         fr=open(scan+"/"+result,'r')
         res_dict=json.load(fr)
         fr.close()
+        if not res_dict[check['cmd']]:
+            nodata.append(res_dict['hostname'])
+            continue
         host=res_dict['hostname']
         tot=len(res_dict[check['cmd']])
         tot_dict[host]=tot
-    text=print_dict(check['desc'], tot_dict)
+    text=print_dict(check['desc'], tot_dict, nodata)
     return text
 
 def basic_stats(scan, check):
@@ -141,10 +169,14 @@ def basic_stats(scan, check):
     vals={}
     stats={}
     totdev=float(len(results))
+    nodata=[]
     for result in results:
         fr=open(scan+"/"+result,'r')
         res_dict=json.load(fr)
         fr.close()
+        if not res_dict[check['cmd']]:
+            nodata.append(res_dict['hostname'])
+            continue
         vals[res_dict['hostname']]=float(res_dict[check['cmd']][list(res_dict[check['cmd']].keys())[0]][check['sfield']].strip("%"))
     sorted_stats = sorted(vals.items(), key=operator.itemgetter(1))
     sorted_reverse_stats = sorted(vals.items(), key=operator.itemgetter(1), reverse=True)
@@ -158,20 +190,24 @@ def basic_stats(scan, check):
     for x in vals:
         tot+=float(vals[x])
     stats["avg"]=tot/totdev
-    text=print_basic_stats(check['desc'],check['unit'], stats)
+    text=print_basic_stats(check['desc'],check['unit'], stats, nodata)
     return text
 
 def empty(scan, check):
     results = os.listdir(scan)
     failed=[]
     failed_detail={}
+    nodata=[]
     for result in results:
         fr=open(scan+"/"+result,'r')
         res_dict=json.load(fr)
         fr.close()
+        if not res_dict[check['cmd']]:
+            nodata.append(res_dict['hostname'])
+            continue
         if len(res_dict[check['cmd']])!=0:
             failed.append(res_dict['hostname'])
             failed_detail[res_dict['hostname']]=[]
             failed_detail[res_dict['hostname']].append(res_dict[check['cmd']])
-    text=print_failures(check['desc'], failed, failed_detail)
+    text=print_failures(check['desc'], failed, failed_detail, nodata)
     return text
