f564fdd6e564f85efb4e0fb312c7c1f6a0c07b49
UmbertoM <umberto.manferdini@gmail.com>
2022-04-11 11:25:15 +0200

    debug

--- gen_report_lib.py
+++ gen_report_lib.py
@@ -138,6 +138,7 @@ def string_equal(scan, check):
                     continue
                 try:
                     for listelem in res_dict[check['cmd']][tested][check['tfield']]:
+                        print(listelem)
                         if listelem!=check['val']:
                             if flag:
                                 failed.append(res_dict['facts']['info']['hostname'])
