306a6c2b1e24f4f1e67199193b1a6e45b35eea02
UmbertoM <umberto.manferdini@gmail.com>
2022-05-30 17:42:06 +0200

    output cosmetic

--- confcheck_lib.py
+++ confcheck_lib.py
@@ -19,63 +19,67 @@ def exec_confcheck(check, conf, fo):
     fo.write("Matches: " + str(len(items)) + "\n")
 
     if len(check)==2 and len(items)>0:
-        fo.write("\t PASS\n")
+        fo.write("\tPASS\n")
 
     #if nothing found check is over
     if len(items)==0:
-        fo.write("\t FAIL\n\n")
+        fo.write("\tFAIL\n\n")
         return
 
     if 'eq' in check:
         fo.write("Check if all the nodes have value " + str(check['eq']) + "\n")
         flag=items[0].text is not None
         if not flag:
-            fo.write("\t FAIL [specified node path has no text]\n")
+            fo.write("\tFAIL [specified node path has no text]\n")
 
         for item in items:
             if item.text is not None:
                 flag=(str(item.text)==str(check['eq']))
                 if not flag:
-                    fo.write("\t FAIL\n")
+                    fo.write("\tFAIL\n")
                     break
 
         if flag:
-            fo.write("\t PASS\n")
+            fo.write("\tPASS\n")
 
     if 'tot' in check:
+        fo.write("Check if the number of occurences is the expected one: " + str(check['tot']) + "\n")
         if len(items)==int(check['tot']):
-            fo.write("\t PASS\n")
+            fo.write("\tPASS\n")
         else:
-            fo.write("\t FAIL [expected matches: " + str(check['tot']) + " ]\n")
+            fo.write("\tFAIL\n")
 
     if 'inspect' in check:
+        fo.write("Inspecting element\n")
         wrng=False
         if 'failid' not in check:
             fo.write("WRNG: failure identifier not found. Please define it.\n")
             wrng=True
         for i in check['inspect']:
             if 'node' in i and 'value' not in i:
+                fo.write("\tNode: " + i['node'] + "\n")
                 for item in items:
                     xpathstr=".//" + i['node']
                     if not item.xpath(xpathstr):
                         if wrng:
-                            fo.write("One item failed. Please check device.\n")
+                            fo.write("\t\tOne item failed. Please check device.\n")
                         if not wrng:
                             fail_l=[]
                             for key in check['failid']:
                                 fail_l.append(item.findtext(key))
                             fail_str=' - '.join(fail_l)
-                            fo.write(fail_str + " failed.\n")
+                            fo.write("\t\t" + fail_str + " failed.\n")
             if 'node' in i and 'value' in i:
+                fo.write("\tNode: " + i['node'] + " , value: " + i['value'] + "\n")
                 for item in items:
                     xpathstr=".//" + i['node'] + "/text()='" + i['value'] + "'"
                     if not item.xpath(xpathstr):
                         if wrng:
-                            fo.write("One item failed. Please check device.\n")
+                            fo.write("\t\tOne item failed. Please check device.\n")
                         if not wrng:
                             fail_l=[]
                             for key in check['failid']:
-                                fail_l.append(item.findtext(key))
+                                fail_l.append("\t\t" + item.findtext(key))
                             fail_str=' - '.join(fail_l)
                             fo.write(fail_str + " failed.\n")
 
