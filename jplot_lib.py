ef7c93d5c77af49b93bfc7fb184c76bdf9c9c645
UmbertoM <umberto.manferdini@gmail.com>
2022-06-03 15:32:11 +0200

    multi simple line plot added

--- jplot_lib.py
+++ jplot_lib.py
@@ -39,3 +39,50 @@ def simple_line_specific(jplot, historic, foldername):
         plt.xticks(rotation=80)
         plt.tight_layout()
         plt.savefig(foldername + "/" + device + "_" + jplot['img_name'] + ".png", bbox_inches = "tight")
+
+def simple_line_multi(jplot, historic, foldername):
+    dev_list=[]
+    if 'devices' in jplot:
+        dev_list = jplot['devices'].copy()
+    else:
+        dev_list = historic.keys().copy()
+
+    for device in dev_list:
+        plots_data={}
+        for scan in historic[device]:
+            try:
+                fi=open(scan,'r')
+                dev_data = yaml.load(fi, Loader=yaml.FullLoader)
+            except Exception as e:
+                print(str(device) + " SCAN: " + str(scan) + ", LOAD SCAN FILE ERROR: " + str(e))
+                return
+            l1=jplot['data'].split("->")[0]
+            l3=jplot['data'].split("->")[1]
+            for l2 in dev_data[str(l1)]:
+                try:
+                    float_verify=float(dev_data[str(l1)][l2][str(l3)])
+                except Exception as e:
+                    print(str(device) + " SCAN: " + str(scan) + ", DATA EXTRACT ERROR: " + str(e))
+                    return
+                if l2 not in plots_data:
+                    plots_data[l2]={}
+                    plots_data[l2]['y']=[]
+                    plots_data[l2]['y'].append(float(dev_data[str(l1)][l2][str(l3)]))
+                    plots_data[l2]['x']=[]
+                    plots_data[l2]['x'].append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
+                else:
+                    plots_data[l2]['y'].append(float(dev_data[str(l1)][l2][str(l3)]))
+                    plots_data[l2]['x'].append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
+        for draw in plots_data:
+            if len(plots_data[draw]['x'])<1:
+                print(str(device) + ", " + l2 + " NO DATA TO PLOT ERROR: " + str(e))
+                continue
+            plt.plt(plots_data[draw]['x'], plots_data[draw]['y'], label=l2)
+        plt.grid()
+        plt.title(jplot['desc'])
+        plt.ylabel(jplot['ylabel'])
+        plt.xlabel("scans")
+        plt.xticks(rotation=80)
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(foldername + "/" + device + "_" + jplot['img_name'] + ".png", bbox_inches = "tight")
