244f5db2065b886b96ecdbaa68072bfaa86bf4cf
UmbertoM <umberto.manferdini@gmail.com>
2022-06-24 12:18:45 +0200

    jplot compare device specific use-case addes plot now include markers new tv for pfe traffic stats

--- jplot_lib.py
+++ jplot_lib.py
@@ -31,7 +31,7 @@ def simple_line_specific(jplot, historic, foldername):
         if len(x)<1:
             print(str(device) + ", NO DATA TO PLOT ERROR: " + str(e))
             return
-        plt.plot(x,y)
+        plt.plot(x,y, linestyle='-', marker='o')
         plt.grid()
         plt.title(jplot['desc'])
         plt.ylabel(jplot['ylabel'])
@@ -78,7 +78,7 @@ def simple_line_multi(jplot, historic, foldername):
             if len(plots_data[draw]['x'])<1:
                 print(str(device) + ", " + l2 + " NO DATA TO PLOT ERROR: " + str(e))
                 continue
-            plt.plot(plots_data[draw]['x'], plots_data[draw]['y'], label=draw)
+            plt.plot(plots_data[draw]['x'], plots_data[draw]['y'], label=draw, linestyle='-', marker='o')
         plt.grid()
         plt.title(jplot['desc'])
         plt.ylabel(jplot['ylabel'])
@@ -88,3 +88,43 @@ def simple_line_multi(jplot, historic, foldername):
         plt.tight_layout()
         plt.savefig(foldername + "/" + device + "_" + jplot['img_name'] + ".png", bbox_inches = "tight")
         plt.clf()
+
+def compare_devices_specific(jplot, historic, foldername):
+    dev_list=[]
+    if 'devices' in jplot:
+        dev_list = jplot['devices'].copy()
+    else:
+        dev_list = historic.keys().copy()
+
+    for device in dev_list:
+        x=[]
+        y=[]
+        for scan in historic[device]:
+            try:
+                fi=open(scan,'r')
+                dev_data = yaml.load(fi, Loader=yaml.FullLoader)
+            except Exception as e:
+                print(str(device) + " SCAN: " + str(scan) + ", LOAD SCAN FILE ERROR: " + str(e))
+                return
+            try:
+                l1=jplot['data'].split("->")[0]
+                l2=jplot['data'].split("->")[1]
+                l3=jplot['data'].split("->")[2]
+                float_verify=float(dev_data[str(l1)][str(l2)][str(l3)])
+            except Exception as e:
+                print(str(device) + " SCAN: " + str(scan) + ", DATA EXTRACT ERROR: " + str(e))
+                return
+            x.append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
+            y.append(float(dev_data[str(l1)][str(l2)][str(l3)]))
+        if len(x)<1:
+            print(str(device) + ", NO DATA TO PLOT ERROR: " + str(e))
+            return
+        plt.plot(x,y, linestyle='-', marker='o')
+    plt.grid()
+    plt.title(jplot['desc'])
+    plt.ylabel(jplot['ylabel'])
+    plt.xlabel("scans")
+    plt.xticks(rotation=80)
+    plt.tight_layout()
+    plt.savefig(foldername + "/" + jplot['img_name'] + ".png", bbox_inches = "tight")
+    plt.clf()
