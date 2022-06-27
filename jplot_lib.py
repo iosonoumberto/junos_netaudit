f71bbcb56e36afe4281fc1c867632b757a67012d
UmbertoM <umberto.manferdini@gmail.com>
2022-06-27 16:52:00 +0200

    all the plots now use numpy arrays and pandas and are ready to deal with missing data

--- jplot_lib.py
+++ jplot_lib.py
@@ -29,13 +29,18 @@ def simple_line_specific(jplot, historic, foldername):
                 float_verify=float(dev_data[str(l1)][str(l2)][str(l3)])
             except Exception as e:
                 print(str(device) + " SCAN: " + str(scan) + ", DATA EXTRACT ERROR: " + str(e))
-                return
+                x.append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
+                y.append(None)
             x.append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
             y.append(float(dev_data[str(l1)][str(l2)][str(l3)]))
         if len(x)<1:
             print(str(device) + ", NO DATA TO PLOT ERROR: " + str(e))
             return
-        plt.plot(x,y, linestyle='-', marker='o')
+        xseries=numpy.array(x).astype(numpy.str)
+        yseries = numpy.array(y).astype(numpy.double)
+        mask = numpy.isfinite(yseries)
+        ypanda = pandas.Series(yseries, index=xseries)
+        plt.plot(ypanda.interpolate(), linestyle='-', marker='o', label=device, markevery=mask)
         plt.grid()
         plt.title(jplot['desc'])
         plt.ylabel(jplot['ylabel'])
@@ -53,7 +58,8 @@ def simple_line_multi(jplot, historic, foldername):
         dev_list = historic.keys().copy()
 
     for device in dev_list:
-        plots_data={}
+        x=[]
+        y=[]
         for scan in historic[device]:
             try:
                 fi=open(scan,'r')
@@ -68,30 +74,29 @@ def simple_line_multi(jplot, historic, foldername):
                     float_verify=float(dev_data[str(l1)][l2][str(l3)])
                 except Exception as e:
                     print(str(device) + " SCAN: " + str(scan) + ", DATA EXTRACT ERROR: " + str(e))
-                    return
-                if l2 not in plots_data:
-                    plots_data[l2]={}
-                    plots_data[l2]['y']=[]
-                    plots_data[l2]['y'].append(float(dev_data[str(l1)][l2][str(l3)]))
-                    plots_data[l2]['x']=[]
-                    plots_data[l2]['x'].append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
-                else:
-                    plots_data[l2]['y'].append(float(dev_data[str(l1)][l2][str(l3)]))
-                    plots_data[l2]['x'].append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
-        for draw in plots_data:
-            if len(plots_data[draw]['x'])<1:
-                print(str(device) + ", " + l2 + " NO DATA TO PLOT ERROR: " + str(e))
-                continue
-            plt.plot(plots_data[draw]['x'], plots_data[draw]['y'], label=draw, linestyle='-', marker='o')
-        plt.grid()
-        plt.title(jplot['desc'])
-        plt.ylabel(jplot['ylabel'])
-        plt.xlabel("scans")
-        plt.xticks(rotation=80)
-        plt.legend()
-        plt.tight_layout()
-        plt.savefig(foldername + "/" + device + "_" + jplot['img_name'] + ".png", bbox_inches = "tight")
-        plt.clf()
+                    x.append(None)
+                    y.append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
+                    continue
+                    y.append(float(dev_data[str(l1)][l2][str(l3)]))
+                    x.append(scan.split("/")[1].split("_audit_")[1].replace("_"," ")[:-5])
+
+        if len(plots_data[draw]['x'])<1:
+            print(str(device) + ", " + l2 + " NO DATA TO PLOT ERROR: " + str(e))
+            continue
+        xseries=numpy.array(x).astype(numpy.str)
+        yseries = numpy.array(y).astype(numpy.double)
+        mask = numpy.isfinite(yseries)
+        ypanda = pandas.Series(yseries, index=xseries)
+        plt.plot(ypanda.interpolate(), linestyle='-', marker='o', label=device, markevery=mask)
+    plt.grid()
+    plt.title(jplot['desc'])
+    plt.ylabel(jplot['ylabel'])
+    plt.xlabel("scans")
+    plt.xticks(rotation=80)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(foldername + "/" + device + "_" + jplot['img_name'] + ".png", bbox_inches = "tight")
+    plt.clf()
 
 def compare_devices_specific(jplot, historic, foldername):
     dev_list=[]
